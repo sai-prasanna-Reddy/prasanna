3609194c76dc4c58c9725a7d13e66aa7887faf34
sai-prasanna-Reddy <62071491+sai-prasanna-Reddy@users.noreply.github.com>
2020-03-16 22:46:49 +0530

    Add files via upload

---
+++ SwitchEx.java
@@ -0,0 +1,18 @@
+public class SwitchEx{  
+public static void main(String[] args) {  
+    int number=35;     
+    switch(number){     
+    case 25: 
+      System.out.println("25");  
+      break;  
+    case 35: 
+      System.out.println("35");  
+      break;  
+    case 45: 
+      System.out.println("45");  
+      break;    
+    default:
+      System.out.println("Not in 25, 35 or 45");  
+    }  
+}  
+}  
