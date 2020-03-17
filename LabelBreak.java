3aebfa4b07dadb23391c90b1ce71c96829df3c6d
sai-prasanna-Reddy <62071491+sai-prasanna-Reddy@users.noreply.github.com>
2020-03-17 09:35:04 +0530

    Add files via upload

---
+++ LabelBreak.java
@@ -0,0 +1,17 @@
+class LabelBreak
+{
+  public static void main(String args[])
+  {
+    prasanna:
+    for(int i=1;i<=4;i++)
+    {
+      for(int j=1;j<=4;j++)
+      {
+        if(i==3)
+           break prasanna;
+        System.out.print("# ");
+       }
+      System.out.println("  ");
+    }
+}
+}
