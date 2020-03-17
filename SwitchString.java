c942511ef6a1343fa6dd217b63b18c031c235b40
sai-prasanna-Reddy <62071491+sai-prasanna-Reddy@users.noreply.github.com>
2020-03-17 08:40:35 +0530

    Add files via upload

---
+++ SwitchString.java
@@ -0,0 +1,18 @@
+class SwitchString
+{
+ public static void main(String args[])
+ {
+   String str="hii";
+   switch(str)
+   { 
+    case "hii":
+       System.out.println("hii");
+       break;
+    case "hello":
+        System.out.println("hello");
+        break;
+    default:
+        System.out.println("nothing");
+     }
+   }
+}
