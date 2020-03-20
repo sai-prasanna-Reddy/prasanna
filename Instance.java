11cdebc82f9e15bda7d838b49bba24d8f38452cc
sai-prasanna-Reddy <62071491+sai-prasanna-Reddy@users.noreply.github.com>
2020-03-20 10:01:44 +0530

    Add files via upload

---
+++ Instance.java
@@ -0,0 +1,13 @@
+class Instance
+{
+ public static void main(String args[])
+ {
+   Abc.show();
+  }
+}class Abc
+{
+ public static void show()
+ {
+   System.out.println("we dont need to instanciate an object");
+  }
+}
