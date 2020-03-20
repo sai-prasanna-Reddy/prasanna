11462378ff9c0d89bf315d8f7bae3b8ddfec4751
sai-prasanna-Reddy <62071491+sai-prasanna-Reddy@users.noreply.github.com>
2020-03-20 10:41:26 +0530

    Add files via upload

---
+++ StaticBlock.java
@@ -0,0 +1,15 @@
+class StaticBlock
+{
+  static
+  {
+    System.out.println("Helllo world");
+  }
+  public static void main(String args[])
+  {
+    System.out.println("this is main method");
+   }
+   static 
+   {
+     System.out.println("this is static method2");
+    }
+}
