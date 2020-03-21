25721b3c30cdcec26a7dd40839df5009d85840e7
sai-prasanna-Reddy <62071491+sai-prasanna-Reddy@users.noreply.github.com>
2020-03-21 11:18:17 +0530

    Add files via upload

---
+++ Gfg.java
@@ -0,0 +1,13 @@
+class Gfg 
+{ 
+    public static void main(String[] args)  
+    { 
+        // a final reference variable sb 
+        final StringBuilder sb = new StringBuilder("prasanna"); 
+          
+        System.out.println(sb);  
+        sb.append("Working for a company"); 
+          
+        System.out.println(sb); 
+    }     
+}
