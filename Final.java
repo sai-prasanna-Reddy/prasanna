dc22e6e4bbcf616e4138f880ebfe58202773f6da
sai-prasanna-Reddy <62071491+sai-prasanna-Reddy@users.noreply.github.com>
2020-03-21 19:51:18 +0530

    Add files via upload

---
+++ Final.java
@@ -0,0 +1,14 @@
+public class Final {
+    public static void main(String[] args)
+    {
+     A obj=new A();
+     obj.show();
+    }
+}
+final class A
+{
+final public void show()
+{
+  System.out.println("in show");
+}
+}
