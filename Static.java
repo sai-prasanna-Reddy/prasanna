76c63eb36cb76f67861b067088ab1143a1f33213
sai-prasanna-Reddy <62071491+sai-prasanna-Reddy@users.noreply.github.com>
2020-03-21 20:19:27 +0530

    Add files via upload

---
+++ Static.java
@@ -0,0 +1,16 @@
+class Static
+{
+  public static void main(String args[])
+  {
+    A2.show();
+   }
+}
+class A2
+{
+  static int i;
+  public static void show()
+  {
+    System.out.println("how are you");
+    System.out.println(i);
+   }
+}
