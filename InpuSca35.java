f7da8300fe714559ee4f317d024fe64828fa338d
sai-prasanna-Reddy <62071491+sai-prasanna-Reddy@users.noreply.github.com>
2020-03-22 12:25:11 +0530

    Add files via upload

---
+++ InpuSca35.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class InpuSca35 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);               //Scanner Class 
+		System.out.println("Enter the Number:");
+		int n=sc.nextInt();
+		System.out.println("The number is "+n);
+	}
+}                                     
